3bf5cb816957b424bf0b03ef37204f59937446c1
Roshaan Aziz <salmanaziz216@gmail.com>
2022-12-22 03:17:01 +0500

    Create User Function

--- pages/dashboard/AddProfile.js
+++ pages/dashboard/AddProfile.js
@@ -4,44 +4,126 @@ import Input from "antd/lib/input/Input";
 import Image from "next/image";
 import { StickerWidgetImgUploadIcon } from "../../config/icon.config";
 import Upload from "antd/lib/upload/Upload";
-import DropdownButton from "antd/lib/dropdown/dropdown-button";
-import MenuItem from "antd/lib/menu/MenuItem";
-import TopbarDropdownStyles from "../../containers/Topbar/TopbarDropdown.styles";
+import { createUser } from "../../src/graphql/mutations";
+import { Button } from "antd";
+import {API,graphqlOperation} from 'aws-amplify'
 export default function AddProfile() {
   const [type, setType] = useState("");
-  const [dropdown,setdropdown]=useState(false)
+  const [User,setUser]=useState({
+    Name:"",
+    Email:"",
+    PhoneNo:0
+  })
+  const [Student,setStudent]=useState({
+    RollNo:"",
+    Qualification:""
+  })
+  const dataHandler=async(event)=>{
+    event.preventDefault()
+    console.log(User)
+    const variables = {
+      data: {
+        rollNumber:Student.RollNo,
+        name: User.Name,
+        qualification:Student.Qualification,
+        image:"vljlkslj",
+        userType:"student"
+      } // key is "input" based on the mutation above
+    };
+    console.log(variables)
+    await API.graphql(graphqlOperation(createUser, variables)).then((result)=>{
+      console.log("Value inserted")
+    }).catch((error)=>{
+      console.log(error)
+      console.log("error")
+    })
+  }
+  const inputHandler=(event)=>{
+    const { name, value } = event.target;
+    setUser({ ...User, [name]: value });
+    console.log(User)
+    setStudent({...Student,[name]:value})
+    console.log(Student)
+  }
   return (
     <DashboardLayout>
-      <div className="w-5/7 h-5/7 my-6 mx-10 bg-white font-extrabold align-middle rounded-md shadow-2xl ">
-        <div className="flex pt-5 pb-0 px-4 ">
+      <div className="w-5/7 h-5/7 my-6 mx-10 bg-white font-extrabold align-middle rounded-md shadow-2xl overflow-hidden">
+        <div className="flex pt-5 pb-0 px-4 overflow-hidden">
           <h1 className="font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-gray-700 to-red-700">
             {" "}
             Add User
           </h1>
         </div>
-        <div className="flex flex-row flex-wrap items-center w-full px-6 py-8 ">
-          <div className="flex-1 flex flex-col w-5/12 justify-start items-center">
-            <Input className="my-2 w-5/6" placeholder="Student Name"></Input>
-            <Input className="my-2 w-5/6" placeholder="Student Email"></Input>
-            <Input className="my-2 w-5/6" type="number" placeholder="Student Phone No"></Input>
+        <div className="flex flex-row items-center w-full px-8 space-x-2 overflow-hidden bg-red">
+          <div className="flex-1 flex flex-col w-[44%] justify-start items-center">
+            <Input className="my-2 " name="Name"  placeholder="Student Name" onChange={inputHandler}></Input>
+            <Input className="my-2 " placeholder="Student Email" name="Email" onChange={inputHandler}></Input>
+            <Input
+              className="my-2 "
+              type="number"
+              placeholder="Student Phone No"
+              onChange={inputHandler}
+            ></Input>
           </div>
-          <div className="flex flex-col items-center w-5/12">
+          <div className="my-2 w-[44%]">
             <Image
               src="https://images.unsplash.com/photo-1464375117522-1311d6a5b81f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2250&q=80"
               alt="Image"
               width={200}
-              height={240}
+              height={160}
               priority
               className="object-contain"
             />
+          </div>
+         
+          {/* <div className="flex flex-col items-center w-1/5 h-full ml-40 overflow-hidden">
+            <div className="mr-90">
+              
+              <label for="cars">Type:</label>
+              <select
+                name="cars"
+                id="cars"
+                onClick={(e) => {
+                  if (e.target.value == "Student") {
+                    setType("Student");
+                  } else if (e.target.value == "Teacher") {
+                    setType("Teacher");
+                  } else {
+                    setType("");
+                  }
+                }}
+              >
+                <option value="Type"></option>
+                <option value="Student">Student</option>
+                <option value="Teacher">Teacher</option>
+              </select>
+              {type === "Student" && (
+                <form className="overflow-hidden">
+                  <Input
+                    className="mt-2 w-5/6"
+                    placeholder="Current Semester"
+                  ></Input>
+                  <Input
+                    className="mt-2 w-5/6"
+                    placeholder="Current Gpa"
+                  ></Input>
+                </form>
+              )}
+            </div>
+          </div> */}
         </div>
-        {
-              <div className="flex flex-row items-center align-center justify-center w-full space-x-6 px-16">
-            <Input className="mb-6 w-4/6" placeholder="Current Semester"></Input>
-            <Input className="mb-6 w-4/6" type="number" placeholder="Currrent CGPA"></Input>
-                  </div>
-            }
-      </div>
+        <div  className="flex px-[26px] space-x-2 flex-wrap justify-left">
+          <Input className="my-2 w-[49%] " type="file"></Input>
+
+          <Input className="my-2 w-[49%]" name="Name" placeholder="Student Name"></Input>
+
+          <Input className="my-2 w-[49%] " name="RollNo"  placeholder="Roll no" onChange={inputHandler}></Input>
+
+          <Input className="my-2 w-[49%]" name="Qualification" placeholder="Qualification" onChange={inputHandler}></Input>
+
+          <Button onClick={dataHandler}>Submit</Button>
+
+          </div>
       </div>
     </DashboardLayout>
   );
